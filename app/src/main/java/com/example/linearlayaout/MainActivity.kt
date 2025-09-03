2cc858b1a60c5426d330a0738ec93e8029124e06
giovannibrtt <giovanni.lima@etec.sp.gov.br>
2025-09-03 08:28:38 -0300

    terminada atividade

---
+++ app/src/main/java/com/example/linearlayaout/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.linearlayaout
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        var email = findViewById<EditText>(R.id.editTextEmail).text
+        var senha = findViewById<EditText>(R.id.editTextSenha).text
+        var btnLogar = findViewById<Button>(R.id.buttonLogin)
+        var btnCadastro = findViewById<Button>(R.id.buttonCadastro)
+
+        btnLogar.setOnClickListener{
+            if (email.toString().equals("email@gmail.com") && senha.toString().equals("123")){
+                var intent = Intent(this,MainActivity2::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Vc moraria nessa casa?",Toast.LENGTH_LONG).show()
+            }
+        }
+        btnCadastro.setOnClickListener{
+            var intent = Intent(this,CadastroActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
